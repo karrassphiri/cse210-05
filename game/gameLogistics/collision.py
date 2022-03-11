ada4c7f1c21690fa0afbfcf78e6b63d35aacaae0
karrassphiri <phi21020@byui.edu>
2022-03-11 16:12:17 +0200

    adding some classes done

--- game/gameLogistics/collision.py
+++ game/gameLogistics/collision.py
@@ -1,4 +1,42 @@
 class Colission:
     """ this is in charge of the collision of the players 
-    
-    Author: Karrass"""
+
+    Author: Karrass
+    """
+
+    def __init__(self, characters, score, points):
+        self._characters = characters
+        self._score = score
+        self._points = points
+
+    def check_collision(self):
+        """Checks if the player is colliding with an object.
+
+        Args:
+            self (self): The cast of characters."""
+
+        score = score(self._points)
+        player = self._characters.get_character("player")[0]
+        gems = self._characters.get_character("gem")
+        rocks = self._characters.get_character("rock")
+
+        for gem in gems:
+            if self.__is_colliding(player, gem):
+                self._score = score.earn_point(self._score)
+
+        for rock in rocks:
+            if self.__is_colliding(player, rock):
+                self._score = score.lose_point(self._score)
+
+    def __is_colliding(self, player, character):
+        """ Evaluates if the player has the same position as the current character
+            return: bool
+        """
+        if character.equals(character.get_position(), player.get_position()):
+            return True
+
+    def get_score(self):
+        """ Gets the score
+            return: int
+        """
+        return self._score
--- game/gameLogistics/keyboard.py
+++ game/gameLogistics/keyboard.py
@@ -1,11 +1,57 @@
+import pyray
+
+
 class Keyboard:
 
-  """
-  Keyboard class is a child class of the Keys class.
-  It will get the inputs of player one and calculate the position of its character by using the keys: WASD
-  to move up (W), down (S), left (A) and right (D)
+    """
+    Keyboard class is a child class of the Keys class.
+    It will get the inputs of player one and calculate the position of its character by using the keys: WASD
+    to move up (W), down (S), left (A) and right (D)
 
-  Attributes:
+    Attributes:
 
-  Author: Karrass
-  """
+    Author: Karrass
+    """
+
+    def __init__(self):
+        """Constructs a new KeyboardService."""
+        self._keys = {}
+
+        self._keys['w'] = pyray.KEY_W
+        self._keys['a'] = pyray.KEY_A
+        self._keys['s'] = pyray.KEY_S
+        self._keys['d'] = pyray.KEY_D
+
+        self._keys['i'] = pyray.KEY_I
+        self._keys['j'] = pyray.KEY_J
+        self._keys['k'] = pyray.KEY_K
+        self._keys['l'] = pyray.KEY_L
+
+    def __init__(self, cell_size=2):
+        """Constructs a new KeyboardService using the specified cell size.
+        """
+        self._size = cell_size
+
+    def get_direction(self):
+        """Gets the selected direction based on the currently pressed keys.
+
+            return: int
+        """
+
+        dx = 0
+        dy = 0
+
+        if pyray.is_key_down(pyray.KEY_LEFT):
+            dx = -1
+
+        if pyray.is_key_down(pyray.KEY_RIGHT):
+            dx = 1
+
+        return dx
+
+    def get_key_board(self):
+        """ It checks the key that was pressed
+
+            return: boolean
+        """
+        return pyray.is_key_down(pyray.KEY_A)
